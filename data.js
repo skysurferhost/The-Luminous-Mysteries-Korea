fc0bacae41ca46434f3a49f05885ca5a79d746dc
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-03-12 21:55:26 -0400

    update

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan",
+      "name": "1. 예수님께서 요르단 강에서 세례받으심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6649957818216627,
+          "pitch": 0.08677225450899329,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6319923911907424,
+          "pitch": -0.17290606185455815,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">세례를 받으시다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">그때에 예수님께서는 요한에게 세례를 받으시려고 갈릴래아에서 요르단으로 그를 찾아가셨다. 그러나 요한은 “제가 선생님께 세례를 받아야 할 터인데 선생님께서 저에게 오시다니요?” 하면서 그분을 말렸다. 예수님께서는 “지금은 이대로 하십시오. 우리는 이렇게 해서 마땅히 모든 의로움을 이루어야 합니다.” 하고 대답하셨다. 그제야 요한이 예수님의 뜻을 받아들였다.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">예수님께서는 세례를 받으시고 곧 물에서 올라오셨다. 그때 그분께 하늘이 열렸다. 그분께서는 하느님의 영이 비둘기처럼 당신 위로 내려오시는 것을 보셨다. 그리고 하늘에서 이렇게 말하는 소리가 들려왔다. “이는 내가 사랑하는 아들, 내 마음에 드는 아들이다.”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마태 3:13–17)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-d-tic-ci-cana",
+      "name": "2. 예수님께서 가나에서 첫 기적을 행하심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20790684707299256,
+          "pitch": -0.008690813679965714,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        },
+        {
+          "yaw": 1.3359429840287653,
+          "pitch": 0.13937177954095148,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24373764674771792,
+          "pitch": -0.2789593316817669,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">카나의 혼인 잔치</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">사흘째 되는 날, 갈릴래아 카나에서 혼인 잔치가 있었는데, 예수님의 어머니도 거기에 계셨다. 예수님도 제자들과 함께 그 혼인 잔치에 초대를 받으셨다. 그런데 포도주가 떨어지자 예수님의 어머니가 예수님께 “포도주가 없구나.” 하였다. 예수님께서 어머니에게 말씀하셨다. “여인이시여, 저에게 무엇을 바라십니까? 아직 저의 때가 오지 않았습니다.” 그분의 어머니는 일꾼들에게 “무엇이든지 그가 시키는 대로 하여라.” 하고 말하였다.</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">거기에는 유다인들의 정결례에 쓰는 돌로 된 물독 여섯 개가 놓여 있었는데, 모두 두세 동이들이였다. 예수님께서 일꾼들에게 “물독에 물을 채워라.” 하고 말씀하셨다. 그들이 물독마다 가득 채우자, 예수님께서 그들에게 다시, “이제는 그것을 퍼서 과방장에게 날라다 주어라.” 하셨다. 그들은 곧 그것을 날라 갔다. 과방장은 포도주가 된 물을 맛보고 그것이 어디에서 났는지 알지 못하였지만, 물을 퍼 간 일꾼들은 알고 있었다. 그래서 과방장이 신랑을 불러 그에게 말하였다. “누구든지 먼저 좋은 포도주를 내놓고, 손님들이 취하면 그보다 못한 것을 내놓는데, 지금까지 좋은 포도주를 남겨 두셨군요.” 이렇게 예수님께서는 처음으로 갈릴래아 카나에서 표징을 일으키시어, 당신의 영광을 드러내셨다. 그리하여 제자들은 예수님을 믿게 되었다.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(요한 2:1-11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi",
+      "name": "3. 예수님께서 하느님 나라를 선포하심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05358848048561171,
+          "pitch": -0.17051613309329028,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        },
+        {
+          "yaw": -1.7029965550555701,
+          "pitch": 0.1012106739874703,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004657148241202336,
+          "pitch": -0.4721606572718109,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">갈릴래아 전도를 시작하시다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">요한이 잡힌 뒤에 예수님께서는 갈릴래아에 가시어, 하느님의 복음을 선포하시며 이렇게 말씀하셨다. “때가 차서 하느님의 나라가 가까이 왔다. 회개하고 복음을 믿어라.”<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마르 1:14-15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-bin-hnh-trn-ni",
+      "name": "4. 예수님께서 거룩하게 변모하심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7953274170598537,
+          "pitch": 0.37459110494686243,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-lp-b-tch-thnh-th"
+        },
+        {
+          "yaw": -1.748073222986461,
+          "pitch": -0.012033693567310877,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.71832037522912,
+          "pitch": -0.1197861019002886,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">영광스러운 모습으로 변모하시다</span><o:p></o:p></p>",
+          "text":  "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">엿새 뒤에 예수님께서 베드로와 야고보와 그의 동생 요한만 따로 데리고 높은 산에 오르셨다. 그리고 그들 앞에서 모습이 변하셨는데, 그분의 얼굴은 해처럼 빛나고 그분의 옷은 빛처럼 하얘졌다. 그때에 모세와 엘리야가 그들 앞에 나타나 예수님과 이야기를 나누었다. 그러자 베드로가 나서서 예수님께 말하였다. “주님, 저희가 여기에서 지내면 좋겠습니다. 원하시면 제가 초막 셋을 지어 하나는 주님께, 하나는 모세께, 또 하나는 엘리야께 드리겠습니다.” 베드로가 말을 채 끝내기도 전에 빛나는 구름이 그들을 덮었다. 그리고 그 구름 속에서, “이는 내가 사랑하는 아들, 내 마음에 드는 아들이니 너희는 그의 말을 들어라.” 하는 소리가 났다. 이 소리를 들은 제자들은 얼굴을 땅에 대고 엎드린 채 몹시 두려워하였다. 예수님께서 다가오시어 그들에게 손을 대시며, “일어나라. 그리고 두려워하지 마라.” 하고 이르셨다. 그들이 눈을 들어 보니 예수님 외에는 아무도 보이지 않았다.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마태 17:1-8)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-lp-b-tch-thnh-th",
+      "name": "5. 예수님께서 성체성사를 세우심",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7996773456601955,
+          "pitch": 0.0118117757142997,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06865999545901147,
+          "pitch": -0.5026090379846924,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">성찬례를 제정하시다</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">그들이 음식을 먹고 있을 때에 예수님께서 빵을 들고 찬미를 드리신 다음, 그것을 떼어 제자들에게 주시며 말씀하셨다. “받아 먹어라. 이는 내 몸이다.” 또 잔을 들어 감사를 드리신 다음 제자들에게 주시며 말씀하셨다. “모두 이 잔을 마셔라. 이는 죄를 용서해 주려고 많은 사람을 위하여 흘리는 내 계약의 피다.<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(마태 26:26-28)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
